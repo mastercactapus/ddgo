c0e372c1ca541ffffc51c7c68d20a1170379cf59
Nathan Caza <mastercactapus@gmail.com>
2016-01-02 13:34:32 -0600

    various fixes
    - source is now opened in read-only mode
    - block device sizes are now properly detected (e.g. block dev as source)
    - warning when dest is too small
    - don't error on truncated output

--- main.go
+++ main.go
@@ -14,15 +14,21 @@ func main() {
 	}
 	src := os.Args[1]
 	dst := os.Args[2]
-	stat, err := os.Stat(src)
-	if err != nil {
-		logrus.Fatalln("stat failed:", err)
-	}
-	fd, err := os.Open(src)
+	fd, err := os.OpenFile(src, os.O_RDONLY, 0)
 	if err != nil {
 		logrus.Fatalln("failed to open src:", err)
 	}
-	bar := pb.New64(stat.Size())
+	defer fd.Close()
+	srcLen, err := fd.Seek(0, 2)
+	if err != nil {
+		logrus.Fatalln(err)
+	}
+	_, err = fd.Seek(0, 0)
+	if err != nil {
+		logrus.Fatalln(err)
+	}
+
+	bar := pb.New64(srcLen)
 	bar.SetUnits(pb.U_BYTES)
 	bar.ShowSpeed = true
 	bar.ShowTimeLeft = true
@@ -31,8 +37,28 @@ func main() {
 	if err != nil {
 		logrus.Fatalln("failed to open dst:", err)
 	}
+	defer out.Close()
+
+	dstLen, err := out.Seek(0, 2)
+	if err != nil {
+		logrus.Fatalln(err)
+	}
+	_, err = out.Seek(0, 0)
+	if err != nil {
+		logrus.Fatalln(err)
+	}
+
+	if dstLen < srcLen {
+		bar.Total = dstLen
+	}
+
 	bar.Start()
-	_, err = io.Copy(io.MultiWriter(bar, out), fd)
+	if dstLen < srcLen {
+		logrus.Warnln("destination device too small, not all bytes will be copied")
+		_, err = io.Copy(io.MultiWriter(bar, out), io.LimitReader(fd, dstLen))
+	} else {
+		_, err = io.Copy(io.MultiWriter(bar, out), fd)
+	}
 	if err != nil {
 		logrus.Fatalln("copy failed:", err)
 	}
